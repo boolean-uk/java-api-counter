2b48fecf033f0006f80f7fbf618e780c2eab0113
Rose Breedveld <rosebreedveld@hotmail.com>
2023-05-23 09:26:00 -0400

    Extensions 1 -3

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,13 +1,20 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("counter")
 public class CounterController {
     private int counter = 0;
+    private HashMap<String, Integer> nameCount;
+    public CounterController() {
+        this.nameCount = new HashMap<>();
+    }
 
     @GetMapping
     public int getCount(){
@@ -26,8 +33,31 @@ public class CounterController {
         return this.counter;
     }
 
+    @GetMapping("custom/{name}")
+    public int customValue(@PathVariable(name = "name") String name){
+        if (!this.nameCount.containsKey(name))  {
+            this.nameCount.put(name, 0);
+            return this.nameCount.get(name);
+        }
+        return this.nameCount.get(name);
+    }
+    @GetMapping("custom/{name}/increment")
+    public int customValueIncrement(@PathVariable(name = "name") String name){
+        if (!this.nameCount.containsKey(name))  {
+            this.nameCount.put(name, 0);
+            return this.nameCount.get(name);
+        }
+        this.nameCount.put(name, nameCount.get(name)+1);
+        return this.nameCount.get(name);
+    }
 
-
-
-
+    @GetMapping("custom/{name}/decrement")
+    public int customValueDecrement(@PathVariable(name = "name") String name){
+        if (!this.nameCount.containsKey(name))  {
+            this.nameCount.put(name, 0);
+            return this.nameCount.get(name);
+        }
+        this.nameCount.put(name, nameCount.get(name)-1);
+        return this.nameCount.get(name);
+    }
 }
