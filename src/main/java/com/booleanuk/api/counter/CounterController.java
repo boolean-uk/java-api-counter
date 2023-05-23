3b64bbadc7d7f775f3c5acaed4a9ca4fb500fa10
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-05-23 14:27:13 +0200

    Initial Counter (get/increment/decrement)

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,28 @@
 package com.booleanuk.api.counter;
 
-public class CounterController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/counter")
+public class CounterController {
+    private int value;
+
+    public CounterController() {
+        this.value = 0;
+    }
+
+    @GetMapping
+    public int getValue() {
+        return this.value;
+    }
+    @GetMapping("/increment")
+    public int incrementValue() {
+        return ++this.value;
+    }
+    @GetMapping("/decrement")
+    public int decrementValue() {
+        return --this.value;
+    }
 }
