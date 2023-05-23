84eb3363e6f4c9eaae18b4cf35f35555af220048
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-23 15:00:53 +0200

    Completed extensions

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,16 +1,22 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
     private int value;
+    Map<String, Integer> nameMap;
 
     public CounterController() {
         this.value = 0;
+        nameMap = new HashMap<>();
     }
 
     @GetMapping
@@ -25,4 +31,37 @@ public class CounterController {
     public int decrementValue() {
         return --this.value;
     }
+
+    @GetMapping("/custom/{name}")
+    public int getCustomValue(@PathVariable (name = "name") String name) {
+        if (this.nameMap.containsKey(name)) {
+            return this.nameMap.get(name);
+        }
+        this.nameMap.put(name, 0);
+        return this.nameMap.get(name);
+    }
+
+    @GetMapping("/custom/{name}/increment")
+    public int incrementCustomValue(@PathVariable (name = "name") String name) {
+        if (this.nameMap.containsKey(name)) {
+            this.nameMap.put(name, this.nameMap.get(name) + 1);
+            return this.nameMap.get(name);
+        }
+        this.nameMap.put(name, 1);
+        return this.nameMap.get(name);
+    }
+    @GetMapping("/custom/{name}/decrement")
+    public int decrementCustomValue(@PathVariable (name = "name") String name) {
+        if (this.nameMap.containsKey(name)) {
+            this.nameMap.put(name, this.nameMap.get(name) - 1);
+            return this.nameMap.get(name);
+        }
+        this.nameMap.put(name, -1);
+        return this.nameMap.get(name);
+    }
+
+
+
+
+
 }
