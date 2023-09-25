29a7845fe4fe94d49cf423d12637ff11af476f29
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-26 00:36:03 +0200

    Added core for API counter

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,32 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/counter")
 public class CounterController {
+    public int counter = 0;
+
+
+    @GetMapping("/increment")
+    public int increment() {
+        counter += 1;
+        return counter;
+    }
+
+    @GetMapping("/decrement")
+    public int decrement() {
+        counter -= 1;
+        return counter;
+    }
+
+
+
 
 }
