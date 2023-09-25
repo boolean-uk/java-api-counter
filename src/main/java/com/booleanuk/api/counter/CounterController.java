2743fe76fef4634d20bc3c2c55c3871c8969a0ec
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-26 00:50:51 +0200

    Added extension to API counter

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -12,6 +12,7 @@ import java.util.Map;
 @RequestMapping("/counter")
 public class CounterController {
     public int counter = 0;
+    private Map<String, Integer> indexVal = new HashMap<>();
 
 
     @GetMapping("/increment")
@@ -27,6 +28,48 @@ public class CounterController {
     }
 
 
+    @GetMapping("/custom/{index}")
+    public int customIndex(@PathVariable(name = "index") String index) {
+        if (indexVal.containsKey(index)) {
+//            int newValue = indexVal.get(index);
+//            newValue++;
+//            indexVal.put(index, newValue);
+//            int returnValue = indexVal.get(index);
 
+            int returnValueExists = indexVal.get(index);
+            return returnValueExists;
+        } else {
+            indexVal.put(index, 0);
 
+            return indexVal.get(index);
+        }
+    }
+
+    @GetMapping("/custom/{index}/increment")
+    public int customIndexIncrement(@PathVariable(name = "index") String index) {
+        if (indexVal.containsKey(index)) {
+            int newValue = indexVal.get(index);
+            newValue++;
+            indexVal.put(index, newValue);
+            int returnValue = indexVal.get(index);
+            return returnValue;
+        } else {
+            indexVal.put(index, 1);
+            return indexVal.get(index);
+        }
+    }
+
+    @GetMapping("/custom/{index}/decrement")
+    public int customIndexDecrement(@PathVariable(name = "index") String index) {
+        if (indexVal.containsKey(index)) {
+            int newValue = indexVal.get(index);
+            newValue--;
+            indexVal.put(index, newValue);
+            int returnValue = indexVal.get(index);
+            return returnValue;
+        } else {
+            indexVal.put(index, -1);
+            return indexVal.get(index);
+        }
+    }
 }
