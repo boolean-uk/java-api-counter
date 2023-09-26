257fb1e3667f92faf5c0200e5fa5ba3f0e3142f4
george georgopoulos <62259101+ggeorgopoulos1998@users.noreply.github.com>
2023-09-26 16:30:12 +0300

    core requirements

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,35 @@
 package com.booleanuk.api.counter;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/counter")
 public class CounterController {
 
+    private int count ;
+    public CounterController(){
+        this.count=0;
+
+    }
+
+
+
+    @GetMapping
+    public int getCount() {
+        return count;
+    }
+
+    @GetMapping("/increment")
+    public int incrementCount() {
+        count++;
+        return count;
+    }
+
+    @GetMapping("/decrement")
+    public int decrementCount() {
+        count--;
+        return count;
+    }
 }
+
