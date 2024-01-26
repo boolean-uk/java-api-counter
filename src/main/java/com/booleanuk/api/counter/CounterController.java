d513be7fe96acbc1e65bc21f55efe96b12203536
hanna <hannaphamm1007@gmail.com>
2024-01-26 13:06:34 +0100

    completed core

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,26 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/counter")
 public class CounterController {
+    private int count = 0;
+
+    @GetMapping
+    public int getCount(){
+        return this.count;
+    }
+    @GetMapping("/increment")
+    public int getIncrement(){
+        return this.count++;
+    }
+    @GetMapping("/decrement")
+    public int getDecrement(){
+        return this.count--;
+    }
+
 
 }
