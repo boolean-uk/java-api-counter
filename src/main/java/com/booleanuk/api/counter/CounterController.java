3393991dd7f30dd57d9c2be63f7a83b959bd076d
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-26 12:38:13 +0100

    Annotate CounterController as RestController and mapping all request to /counter to it

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,10 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/counter")
 public class CounterController {
 
 }
