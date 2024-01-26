e4ed33033bd7213902eb5e9342ccb38780ef9264
Sandersaether <sander.sather@gmail.com>
2024-01-26 13:11:25 +0100

    fixed missing code

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -10,6 +10,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
+    private int counter = 0;
+
+    @GetMapping
+    public int getCounter() {
+        return counter;
+    }
+
+    @GetMapping("/increment")
+    public int incrementCounter() {
+        counter++;
+        return counter;
+    }
+
+    @GetMapping("/decrement")
+    public int decrementCounter() {
+        counter--;
+        return counter;
+    }
 
     private Map<String, Integer> counters = new HashMap<>();
 
