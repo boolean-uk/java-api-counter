c951bddcefdd71bf923d9e8ecb00a5711a724e65
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-26 13:03:19 +0100

    Extension exercise done

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,17 +1,22 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("counter")
 public class CounterController {
     private int counterValue;
+    private HashMap<String, Integer> customCounters;
 
     public CounterController()
     {
         this.counterValue = 0;
+        this.customCounters = new HashMap<>();
     }
 
     @GetMapping
@@ -32,5 +37,42 @@ public class CounterController {
     {
         this.counterValue -= 1;
         return this.counterValue;
+    }
+
+    @GetMapping("custom/{name}")
+    public int getCustomCounter(@PathVariable String name)
+    {
+        if(this.customCounters.containsKey(name))
+            return this.customCounters.get(name);
+        else {
+            this.customCounters.put(name, 0);
+            return this.customCounters.get(name);
+        }
+    }
+
+    @GetMapping("custom/{name}/increment")
+    public int incrementCustomCounter(@PathVariable String name)
+    {
+        if(this.customCounters.containsKey(name)) {
+            this.customCounters.put(name,this.customCounters.get(name)+1);
+            return this.customCounters.get(name);
+        }
+        else {
+            this.customCounters.put(name, 1);
+            return this.customCounters.get(name);
+        }
+    }
+
+    @GetMapping("custom/{name}/decrement")
+    public int decrementCustomCounter(@PathVariable String name)
+    {
+        if(this.customCounters.containsKey(name)) {
+            this.customCounters.put(name,this.customCounters.get(name)-1);
+            return this.customCounters.get(name);
+        }
+        else {
+            this.customCounters.put(name, -1);
+            return this.customCounters.get(name);
+        }
     }
 }
