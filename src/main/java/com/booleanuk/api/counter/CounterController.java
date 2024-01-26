d02461cb9053527c1fd84a2b1e83478202e63d25
msg <mats.gravem@no.experis.com>
2024-01-26 12:42:53 +0100

    Core

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,39 @@
 package com.booleanuk.api.counter;
 
-public class CounterController {
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Data
+@Slf4j
+@RestController
+@RequestMapping("counter")
+public class CounterController {
+    private static int AMOUNT = 1;
+    private int number;
+
+    public CounterController() {
+        this.number = 0;
+    }
+
+    @GetMapping
+    public int getNumber() {
+        return number;
+    }
+
+    @GetMapping("increment")
+    public int incrementNumber() {
+        number += AMOUNT;
+        log.info("Increasing number by " + AMOUNT + ". New number: " + number);
+        return number;
+    }
+
+    @GetMapping("decrement")
+    public int decrementNumber() {
+        number -= AMOUNT;
+        log.info("Decreasing number by " + AMOUNT + ". New number: " + number);
+        return number;
+    }
 }
