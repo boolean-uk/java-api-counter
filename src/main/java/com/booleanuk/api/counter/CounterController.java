42c6159d983839c1d95c26991b7574a45d61e289
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-01-26 13:10:51 +0100

    Finished Extension

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -2,13 +2,17 @@ package com.booleanuk.api.counter;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
     private int number = 0;
+    private HashMap<String, Integer> counters = new HashMap<>();
 
     @GetMapping("/increment")
     public int increment() {
@@ -23,6 +27,35 @@ public class CounterController {
     @GetMapping("/getnumber")
     public int getNumber() {
         return number;
+    }
+
+    @GetMapping("/custom/{name}")
+    public int getCounter(@PathVariable String name) {
+        if(counters.containsKey(name)) {
+            return counters.get(name);
+        }
+        counters.put(name, 0);
+        return counters.get(name);
+    }
+
+    @GetMapping("/custom/{name}/increment")
+    public int incrementCounter(@PathVariable String name) {
+        if(counters.containsKey(name)) {
+            counters.put(name, counters.get(name) + 1);
+            return counters.get(name);
+        }
+        counters.put(name, 1);
+        return counters.get(name);
+    }
+
+    @GetMapping("/custom/{name}/decrement")
+    public int decrementCounter(@PathVariable String name) {
+        if(counters.containsKey(name)) {
+            counters.put(name, counters.get(name) - 1);
+            return counters.get(name);
+        }
+        counters.put(name, -1);
+        return counters.get(name);
     }
 
 }
