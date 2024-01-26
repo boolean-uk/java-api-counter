dcc81697aca779e5e53cd002af5ae8fada13748b
Sandersaether <sander.sather@gmail.com>
2024-01-26 12:19:01 +0100

    core finished

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,29 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/counter")
 public class CounterController {
 
+    private int counter = 0;
+
+    @GetMapping
+    public int getCounter() {
+        return counter;
+    }
+
+    @GetMapping("/increment")
+    public int incrementCounter() {
+        counter++;
+        return counter;
+    }
+
+    @GetMapping("/decrement")
+    public int decrementCounter() {
+        counter--;
+        return counter;
+    }
 }
