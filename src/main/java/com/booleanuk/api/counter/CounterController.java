dcaed43be6bfe5255ac78e17445baa54f099102e
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-01-26 13:36:45 +0100

    Finished core and extension task

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,47 @@
 package com.booleanuk.api.counter;
 
-public class CounterController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/counter/custom/{name}")
+public class CounterController {
+    Counter counter;
+    private HashMap<String, Counter> counters;
+
+    public CounterController() {
+        this.counter = new Counter();
+        this.counters = new HashMap<>();
+    }
+
+    @GetMapping
+    public String getCounter(@PathVariable String name) {
+        if(!this.counters.containsKey(name)) {
+            Counter counter = new Counter();
+            this.counters.put(name, counter);
+        }
+        return this.counters.get(name).getCounter();
+    }
+
+    @GetMapping("/increment")
+    public String incrementCounter(@PathVariable String name) {
+        if(!this.counters.containsKey(name)) {
+            Counter counter = new Counter();
+            this.counters.put(name, counter);
+        }
+        return counters.get(name).incrementCounter();
+    }
+
+    @GetMapping("/decrement")
+    public String decrementCounter(@PathVariable String name) {
+        if(!this.counters.containsKey(name)) {
+            Counter counter = new Counter();
+            this.counters.put(name, counter);
+        }
+        return counters.get(name).decrementCounter();
+    }
 }
