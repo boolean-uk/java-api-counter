899edac9dc7d5282492f65b990f679be480e8511
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-26 13:03:46 +0100

    Fixed extension 1

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,13 +1,21 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
     private int number;
+    private HashMap<String, Integer> namesAndNumbers;
+
+    public CounterController() {
+        this.namesAndNumbers = new HashMap<>();
+    }
 
     @GetMapping
     public int getNumber() {
@@ -24,5 +32,14 @@ public class CounterController {
     public int decrementAndGetNumber() {
         this.number--;
         return this.number;
+    }
+
+    @GetMapping("/custom/{name}")
+    public int getNameNumber(@PathVariable String name) {
+        if(namesAndNumbers.containsKey(name)) {
+            return namesAndNumbers.get(name);
+        }
+        namesAndNumbers.put(name, 0);
+        return 0;
     }
 }
