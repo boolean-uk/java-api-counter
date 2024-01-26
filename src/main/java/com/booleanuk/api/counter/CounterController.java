c39934cb068f7548b110553584893f524df278b5
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-26 13:05:13 +0100

    Add core

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,35 @@
 package com.booleanuk.api.counter;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/counter")
 public class CounterController {
+
+    private int counterValue = 0;
+
+    @GetMapping
+    public String getCounterValue() {
+        return "Value of counter: " + this.counterValue;
+    }
+
+
+    @GetMapping("/increment")
+    public String incrementCounter() {
+        this.counterValue += 1;
+        return "Value of counter: " + this.counterValue;
+    }
+
+
+    @GetMapping("/decrement")
+    public String decrementCounter() {
+        this.counterValue -= 1;
+        return "Value of counter: " + this.counterValue;
+    }
+
 
 }
