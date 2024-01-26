76a47fada213d63f369d036b18fe357005acb00d
Petros Mylonas <petrosmylonascs@gmail.com>
2024-01-26 13:48:30 +0200

    Extension

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,15 +1,19 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
 
     private int counter = 0;
-
+    private Map<String, Integer> customCounters = new HashMap<>();
     @GetMapping
     public int getCounter() {
         return counter;
@@ -25,5 +29,26 @@ public class CounterController {
     public int decrementCounter() {
         counter--;
         return counter;
+    }
+
+    @GetMapping("/custom/{name}")
+    public int getCustomCounter(@PathVariable String name) {
+        return customCounters.getOrDefault(name, 0);
+    }
+
+    @GetMapping("/custom/{name}/increment")
+    public int incrementCustomCounter(@PathVariable String name) {
+        int value = customCounters.getOrDefault(name, 0);
+        value++;
+        customCounters.put(name, value);
+        return value;
+    }
+
+    @GetMapping("/custom/{name}/decrement")
+    public int decrementCustomCounter(@PathVariable String name) {
+        int value = customCounters.getOrDefault(name, 0);
+        value--;
+        customCounters.put(name, value);
+        return value;
     }
 }
