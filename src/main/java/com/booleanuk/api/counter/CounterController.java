425b62362f2a1ae90b7b1ec12ee8a069b440f72f
msg <mats.gravem@no.experis.com>
2024-01-26 13:04:55 +0100

    Extension

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -3,19 +3,25 @@ package com.booleanuk.api.counter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @Data
 @Slf4j
 @RestController
 @RequestMapping("counter")
 public class CounterController {
+    private static int INITIAL_AMOUNT = 0;
     private static int AMOUNT = 1;
+    HashMap<String, Integer> individualCounters;
     private int number;
 
     public CounterController() {
-        this.number = 0;
+        this.individualCounters = new HashMap<>();
+        this.number = INITIAL_AMOUNT;
     }
 
     @GetMapping
@@ -35,5 +41,33 @@ public class CounterController {
         number -= AMOUNT;
         log.info("Decreasing number by " + AMOUNT + ". New number: " + number);
         return number;
+    }
+
+    @GetMapping("custom/{name}")
+    public int getIndividualCounter(@PathVariable String name) {
+        Integer currentValue = individualCounters.putIfAbsent(name, INITIAL_AMOUNT);
+        if (currentValue == null) {
+            log.info("Counter created for " + name + ".");
+            currentValue = individualCounters.get(name);
+        }
+        return currentValue;
+    }
+
+    @GetMapping("custom/{name}/increment")
+    public int incrementIndividualCounter(@PathVariable String name) {
+        getIndividualCounter(name);
+        individualCounters.put(name, individualCounters.get(name) + AMOUNT);
+        int increasedNumber = individualCounters.get(name);
+        log.info("Increasing " + name + " counter by " + AMOUNT + ". New number: " + increasedNumber);
+        return increasedNumber;
+    }
+
+    @GetMapping("custom/{name}/decrement")
+    public int decrementIndividualCounter(@PathVariable String name) {
+        getIndividualCounter(name);
+        individualCounters.put(name, individualCounters.get(name) - AMOUNT);
+        int decreasedNumber = individualCounters.get(name);
+        log.info("Decreasing " + name + " counter by " + AMOUNT + ". New number: " + decreasedNumber);
+        return decreasedNumber;
     }
 }
