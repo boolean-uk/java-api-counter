32d5f318195638a7af6a66849bd8d310a9f0f2e4
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-26 12:59:15 +0100

    counter with value 0 implemented and function getCounter

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,22 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("counter")
 public class CounterController {
+    private int counter;
+
+    public CounterController(){
+        this.counter = 0;
+    }
+
+    @GetMapping
+    public int getCounter(){
+        return this.counter;
+    }
 
 }
