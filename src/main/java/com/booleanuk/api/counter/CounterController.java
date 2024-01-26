f6eb43d34a361e3772d2579a3245dc5ea7a0ddeb
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-01-26 12:52:05 +0100

    Fixed core

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,27 @@
 package com.booleanuk.api.counter;
 
-public class CounterController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/counter")
+public class CounterController {
+    private int number;
+
+    public CounterController() {
+        number = 0;
+    }
+
+    @GetMapping("/increment")
+    public int incrementAndGetNumber() {
+        this.number++;
+        return this.number;
+    }
+
+    @GetMapping("/decrement")
+    public int decrementAndGetNumber() {
+        this.number--;
+        return this.number;
+    }
 }
