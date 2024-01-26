ac2958e9faf5955839d93f8de8bfe94ca59c2c2e
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-26 12:48:18 +0100

    Add counterMap and create getCustomCounter

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,16 +1,22 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/counter")
 public class CounterController {
     private int counter;
+    private Map<String, Integer> counterMap;
 
     public CounterController() {
         this.counter = 0;
+        this.counterMap = new HashMap<>();
     }
 
     @GetMapping
@@ -28,5 +34,14 @@ public class CounterController {
     public int getCounterDecrement() {
         this.counter--;
         return this.counter;
+    }
+
+    @GetMapping("custom/{name}")
+    public int getCustomCounter(@PathVariable String name) {
+        if (this.counterMap.containsKey(name)) {
+            return this.counterMap.get(name);
+        }
+        this.counterMap.put(name, 0);
+        return -1;
     }
 }
